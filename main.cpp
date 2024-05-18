ce34286dc69ebfe3530d727913b3aafba939bb34
William Dang <williamdini@ucr-secure-45-10-13-230-224.wnet.ucr.edu>
2024-05-18 00:50:37 -0700

    Added Basic Main Stuff

--- main.cpp
+++ main.cpp
@@ -3,8 +3,24 @@
 using namespace std;
 
 int main() {
+  int features, algorNumIn;
+  float accuracyPerc = 0.0;
 
+  cout << "Welcome to Drizzay's Feature Selection Algorithm" << endl << endl;
+  cout << "Please enter the total number of features: ";
+  cin >> features;
+  cout << endl;
 
+  cout << "Type the number of the algorithm you want to run.\n\n"
+       << "\tFoward Selection\n"
+       << "\tBackward Elimination\n"
+       << "\tBertie's Special Algorithm\n\n" << endl;
+  cin >> algorNumIn;
+  cout << endl;
+
+  cout << "Using no features and \"random\" evaluation, I get the accuracy of " << accuracyPerc << "%" << endl << endl;
+
+  cout << "Beginning Search." << endl << endl;
 
 
 
